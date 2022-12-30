7a4678911b7863cfb89fe3934c645db49af25973
Ashish <ashishndiitr@gmail.com>
2022-12-30 23:02:59 +0530

    basic ips

---
+++ clientgo_outsidecluster/app2.py
@@ -0,0 +1,12 @@
+#equivalent to kubectl get pods --all-namespaces
+
+from kubernetes import client, config
+
+#loads cubeconfig file in .cube directory
+config.load_kube_config()
+
+#core v1 api to list npods for all namespaces
+v1 = client.CoreV1Api()
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
